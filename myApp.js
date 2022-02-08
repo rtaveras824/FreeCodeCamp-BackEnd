db1a7183a3b6dc34d0ec863259d184996f7d5854
rtaveras824 <rtaveras824@hotmail.com>
2022-02-07 19:44:48 -0500

    Added Update

--- myApp.js
+++ myApp.js
@@ -66,8 +66,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const removeById = (personId, done) => {
