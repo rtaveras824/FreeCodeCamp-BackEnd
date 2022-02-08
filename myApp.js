0a2bd27794a0349ea530c1b724ec9232abb91ddd
rtaveras824 <rtaveras824@hotmail.com>
2022-02-07 20:11:24 -0500

    Added query chain

--- myApp.js
+++ myApp.js
@@ -89,11 +89,7 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({favoriteFoods: foodToSearch}, (err, data) => {
-    if (err) console.error(err);
-    console.log("FOUND");
-    done(null, data);
-  });
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 'asc'}).limit(2).select('-age').exec(done(err, data));
 };
 
 /** **Well Done !!**
