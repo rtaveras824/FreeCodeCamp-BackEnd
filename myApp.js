3ecacc4ba217aacd2b501e264c5ed39e9a5da197
rtaveras824 <rtaveras824@hotmail.com>
2022-02-07 19:04:35 -0500

    Added stuff

--- myApp.js
+++ myApp.js
@@ -38,7 +38,8 @@ const findPeopleByName = (personName, done) => {
 
 const findOneByFood = (food, done) => {
   Person.findOne({ food: food }, (err, data) => {
-    done(null /*, data*/);
+    console.log(data);
+    done(null, data);
   });
 };
 
